a8f6c151cc553b4022e657bf6de6eb1a6c5c1261
ajogious <ajogious@gmail.com>
2026-01-03 21:35:38 +0100

    chore: initialize contact management project

---
+++ src/main/java/com/a3tech/contact_backend/ContactBackendApplication.java
@@ -0,0 +1,13 @@
+package com.a3tech.contact_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/a3tech/contact_backend/ContactBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a3tech.contact_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
